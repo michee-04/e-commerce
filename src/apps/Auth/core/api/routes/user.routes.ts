cc2df98c93d094a870d39ed116c28102a28d21ed
michee_04 <nangmamichee0@gmail.com>
2024-12-25 16:11:09 +0000

    feat: implementation des routes pour les utilisateurs

--- src/apps/Auth/core/api/routes/user.routes.ts
+++ src/apps/Auth/core/api/routes/user.routes.ts
@@ -7,4 +7,18 @@ router.post('/register', UserController.register);
 
 router.post('/verify-account', UserController.verifyAccount);
 
+router.post('/generate-login-otp', UserController.generateLoginOtp);
+
+router.post('/login-with-otp', UserController.loginWithOtp);
+
+router.post('/login-with-password', UserController.loginWithPassword);
+
+router.post('/refresh-token', UserController.refreshToken);
+
+router.post('/forgot-password', UserController.forgotPassword);
+
+router.post('/logout', UserController.logout);
+
+router.post('/reset-password', UserController.resetPassword);
+
 export default router;
