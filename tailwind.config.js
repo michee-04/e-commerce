e60bf8efaa38de3c9a4f5faaa288a3bc97ebae26
komi etsru <komietsru1@gmail.com>
2025-01-03 18:18:01 +0000

    feat(frontend): Ajout des répertoires de base pour le front-end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/public/templates/**/*.{html,js}",
-    "./views/**/*.ejs",
-    "./public/**/*.js",
+    "./src/public/templates/**/*.{html,ejs,js}",
+    "./views/**/*.ejs",           
+    "./public/**/*.js",       
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
